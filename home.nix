50b0761b5c1f8986b862c7aaba5b920123a1fa15
PositiveA9 <cole_hassl411@ahschools.us>
2026-02-04 08:38:36 -0600

    NixOS rebuild on (none) - 2026-02-04 08:38:36

--- home.nix
+++ home.nix
@@ -15,6 +15,113 @@
 	gtk.enable = true;
         };
 	
+	programs.fastfetch = {
+		enable = true;
+		settings = {
+			logo = {
+				source = "~/nixos/assets/icons/nix-lavender.png";
+				padding = {
+					top = 2;
+					left = 3;
+				};
+				width = 40;
+			};
+			modules = [
+				"break"
+				{
+					type = "custom";
+					format = "──────────────────────Hardware──────────────────────";
+				}
+
+				{
+					type = "cpu";
+					key = " ";
+					showPeCoreCount = true;
+					keyColor = "33";
+				}
+				{
+					type = "gpu";
+					key = " 󰍛";
+					keyColor = "33";
+				}
+				{
+					type = "memory";
+					key = " ";
+					keyColor = "33";
+				}
+				{
+					type = "custom";
+					format = "────────────────────────────────────────────────────";
+				}
+				"break"
+				{
+					type = "custom";
+					format = "──────────────────────Software──────────────────────";
+				}
+				{
+					type = "os";
+					key = " 󱄅";
+					keyColor = "yellow";
+				}
+				{
+					type = "kernel";
+					key = " ";
+					keyColor = "yellow";
+				}
+				{
+					type = "packages";
+					key = " ";
+					keyColor = "yellow";
+				}
+				{
+					type = "wm";
+					key = " 󰇄";
+					keyColor = "33";
+				}
+				{
+					type = "lm";
+					key = " 󰍂";
+					keyColor = "33";
+				}
+				{
+					type = "terminal";
+					key = " ";
+					keyColor = "33";
+				}
+				{
+					type = "shell";
+					key = " ";
+					keyColor = "33";
+				}
+				{
+					type = "custom";
+					format = "────────────────────────────────────────────────────";
+				}
+				"break"
+				{
+					type = "custom";
+					format = "────────────────────Uptime / Age────────────────────";
+				}
+				{
+					type = "command";
+					key = "  OS Age ";
+					keyColor = "33";
+					text = "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days";
+				}
+				{
+					type = "uptime";
+					key = "  Uptime ";
+					keyColor = "33";
+				}
+				{
+					type = "custom";
+					format = "────────────────────────────────────────────────────";
+				}
+				"break"
+			];
+		};
+	};
+
 	programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -61,29 +168,6 @@
 		bold.family = "JetBrains Mono Nerd Font"
 		italic.family = "JetBrains Mono Nerd Font"
 		bold_italic.family = "JetBrains Mono Nerd Font"
-	'';
-
-	home.file.".config/fastfetch/config.jsonc".text = ''
-		{
-			"modules": [
-				"Title",
-				"Separator",
-				"OS",
-				"Host",
-				"Kernel",
-				"Uptime",
-				"Packages",
-				"Shell",
-				"Display",
-				"WM",
-				"Terminal",
-				"CPU",
-				"GPU",
-				"Memory",
-				"Swap",
-				"Disk"
-			]
-		}
 	'';
 
 }
